acd0bfe9534f2f5d247e46978cc4e3687bd5a40f
junyatakechi <takechi48j@gmail.com>
2023-05-28 10:31:37 +0900

    追加: ガイドラインstruct

--- contracts/SmArtworksContract.sol
+++ contracts/SmArtworksContract.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 import "./struct/Artwork.sol";
+import "./struct/Guideline.sol";
 
 //
 contract SmArtworksContract {
 
     uint256 private currentWorkId;
     mapping(uint256 => Artwork) public works;
+
+    uint256 public currentVersion;
+    mapping(uint256 => Guideline) public guidelines;
 
     function addWork(
         string memory _title, 
@@ -41,6 +45,21 @@ contract SmArtworksContract {
         require(works[_workId].deactivatedAt == 0, "This Artwork is inactive.");
         return works[_workId];
     }
-    
+
+
+    function addGuideline(string memory _url, string memory _digest) external {
+        currentVersion++;
+        guidelines[currentVersion] = Guideline(_url, _digest, block.timestamp);
+    }
+
+    function getCurrentGuideline() public view returns (Guideline memory) {
+        require(currentVersion > 0, "No guideline is available.");
+        return guidelines[currentVersion];
+    }
+
+    function getGuideline(uint256 version) external view returns (Guideline memory) {
+        return guidelines[version];
+    }
+
 
 }
---
+++ contracts/struct/Guideline.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+struct Guideline {
+    string url;
+    string digest;
+    uint256 updatedAt;
+}
