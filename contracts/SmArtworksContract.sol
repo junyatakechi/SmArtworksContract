5d08216552d473a1f315fb19678778c775ca1562
junyatakechi <takechi48j@gmail.com>
2023-05-28 10:37:22 +0900

    追加: Ownable

--- contracts/SmArtworksContract.sol
+++ contracts/SmArtworksContract.sol
@@ -1,16 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./struct/Artwork.sol";
 import "./struct/Guideline.sol";
 
 //
-contract SmArtworksContract {
+contract SmArtworksContract is Ownable{
 
     uint256 private currentWorkId;
     mapping(uint256 => Artwork) public works;
 
     uint256 public currentVersion;
     mapping(uint256 => Guideline) public guidelines;
+
+    function getWork(uint256 _workId) public view returns (Artwork memory) {
+        require(works[_workId].deactivatedAt == 0, "This Artwork is inactive.");
+        return works[_workId];
+    }
+
+    function getCurrentGuideline() public view returns (Guideline memory) {
+        require(currentVersion > 0, "No guideline is available.");
+        return guidelines[currentVersion];
+    }
+
+    function getGuideline(uint256 version) external view returns (Guideline memory) {
+        return guidelines[version];
+    }
+
+    /// For Artist /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     function addWork(
         string memory _title, 
@@ -20,7 +37,7 @@ contract SmArtworksContract {
         uint256 _minValue,
         uint256 _maxValue,
         uint256 _maxDate
-    ) external {
+    ) external onlyOwner{
         currentWorkId++;
         Artwork memory newWork = Artwork({
             title: _title,
@@ -36,30 +53,14 @@ contract SmArtworksContract {
         works[currentWorkId] = newWork;
     }
 
-    function deactivateWork(uint256 _workId) external {
+    function deactivateWork(uint256 _workId) external onlyOwner {
         require(works[_workId].deactivatedAt == 0, "This Artwork is already inactive.");
         works[_workId].deactivatedAt = block.timestamp;
     }
 
-    function getWork(uint256 _workId) public view returns (Artwork memory) {
-        require(works[_workId].deactivatedAt == 0, "This Artwork is inactive.");
-        return works[_workId];
-    }
-
-
-    function addGuideline(string memory _url, string memory _digest) external {
+    function addGuideline(string memory _url, string memory _digest) external onlyOwner {
         currentVersion++;
         guidelines[currentVersion] = Guideline(_url, _digest, block.timestamp);
     }
-
-    function getCurrentGuideline() public view returns (Guideline memory) {
-        require(currentVersion > 0, "No guideline is available.");
-        return guidelines[currentVersion];
-    }
-
-    function getGuideline(uint256 version) external view returns (Guideline memory) {
-        return guidelines[version];
-    }
-
 
 }
