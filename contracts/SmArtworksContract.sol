f23b56c10ec64e0d00e7e4bb298cecd7686e389d
junyatakechi <takechi48j@gmail.com>
2023-05-28 14:04:09 +0900

    追加: 検証系

--- contracts/SmArtworksContract.sol
+++ contracts/SmArtworksContract.sol
@@ -4,6 +4,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./struct/Artwork.sol";
 import "./struct/Guideline.sol";
 
@@ -71,43 +72,8 @@ contract SmArtworksContract is ERC721, Ownable{
         ));
     }
 
-    // metadata for NFT
-    function _createSecondCreativeRequest(
-        uint256 _workId,
-        address _signerAddress,
-        string memory _signerName,
-        string memory _purpose,
-        string memory _location,
-        uint256 _startDate,
-        uint256 _endDate,
-        uint256 _value,
-        uint256 _guildLineVerId,
-        string memory _signature  // Signature has been added
-    ) internal view returns (string memory){
-        Artwork memory artwork = getWork(_workId);
-        string memory applicationAddress = Strings.toHexString(uint256(uint160(address(this))));
-        string memory signerAddress = Strings.toHexString(uint256(uint160(_signerAddress)));
-        string memory applicationId = "TODO";  // Update with actual value
+    function mint() external{
 
-        return string(abi.encodePacked(
-            "{",
-            '"name": "', artwork.title, '",',
-            '"discription": "', 'Description Here', '",',
-            '"image": "', artwork.mediaURL, '",',
-            '"applicationAddress": "', applicationAddress, '",',
-            '"applicationId": "', applicationId, '",',
-            '"workId":', Strings.toString(_workId), ',',
-            '"signerName": "', _signerName, '",',
-            '"signerAddress": "', signerAddress, '",',
-            '"purpose": "', _purpose, '",',
-            '"location": "', _location, '",',
-            '"startDate":', Strings.toString(_startDate), ',',
-            '"endDate":', Strings.toString(_endDate), ',',
-            '"value":', Strings.toString(_value), ',',
-            '"guildLineVerId": "', Strings.toString(_guildLineVerId), '",',
-            '"signature": "', _signature, '"',
-            "}"
-        ));
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
@@ -142,6 +108,49 @@ contract SmArtworksContract is ERC721, Ownable{
         ));
 
         return dataURI;
+    }
+
+    
+   // 検証
+    function extractSigner(
+        string memory creativeAgreement,
+        bytes memory signature
+    ) public pure returns (address) {
+        // signatureの長さが65バイトであることを確認します。
+        require(signature.length == 65, "Invalid signature length");
+
+        // signatureからv, r, sを抽出します。
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        //
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+
+        // vが27または28であることを確認し、適切な値に設定します。
+        if (v < 27) {
+            v += 27;
+        }
+
+        require(v == 27 || v == 28, "Invalid v value");
+
+        // メッセージハッシュを計算します。
+        // ERC-191: フロント側でも同じ規格を使用する。
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(
+                "\x19Ethereum Signed Message:\n",
+                Strings.toString(uint256(bytes(creativeAgreement).length)),
+                message
+            )
+        );
+
+        // 署名の検証を行い、結果を返します。
+        address recoveredSigner = ECDSA.recover(messageHash, v, r, s);
+        return recoveredSigner;
     }
 
     // burn
@@ -239,6 +248,45 @@ contract SmArtworksContract is ERC721, Ownable{
         // Move the last element to the indexToRemove and then pop the last element
         ownedTokens[indexToRemove] = ownedTokens[ownedTokens.length - 1];
         ownedTokens.pop();
+    }
+
+    // metadata for NFT
+    function _createSecondCreativeRequest(
+        uint256 _workId,
+        address _signerAddress,
+        string memory _signerName,
+        string memory _purpose,
+        string memory _location,
+        uint256 _startDate,
+        uint256 _endDate,
+        uint256 _value,
+        uint256 _guildLineVerId,
+        string memory _signature  // Signature has been added
+    ) internal view returns (string memory){
+        Artwork memory artwork = getWork(_workId);
+        string memory applicationAddress = Strings.toHexString(uint256(uint160(address(this))));
+        string memory signerAddress = Strings.toHexString(uint256(uint160(_signerAddress)));
+        string memory applicationId = "TODO";  // Update with actual value
+
+        return string(abi.encodePacked(
+            "{",
+            '"name": "', artwork.title, '",',
+            '"discription": "', 'Description Here', '",',
+            '"image": "', artwork.mediaURL, '",',
+            '"applicationAddress": "', applicationAddress, '",',
+            '"applicationId": "', applicationId, '",',
+            '"workId":', Strings.toString(_workId), ',',
+            '"signerName": "', _signerName, '",',
+            '"signerAddress": "', signerAddress, '",',
+            '"purpose": "', _purpose, '",',
+            '"location": "', _location, '",',
+            '"startDate":', Strings.toString(_startDate), ',',
+            '"endDate":', Strings.toString(_endDate), ',',
+            '"value":', Strings.toString(_value), ',',
+            '"guildLineVerId": "', Strings.toString(_guildLineVerId), '",',
+            '"signature": "', _signature, '"',
+            "}"
+        ));
     }
 
 }
